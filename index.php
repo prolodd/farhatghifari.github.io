fefb4532a814dc450eb3b63d323105100b40aaf4
prolodd <123445315+prolodd@users.noreply.github.com>
2024-02-12 11:33:52 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,25 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My first website</title>
+</head>
+<body>
+    <?php include 'header.php'; ?>
+    <br>
+    <br>
+    <a href="circle.php">
+        <img src="images/circle.png" height="150px" width= "200px" alt="Go to circle calculator page">
+    </a>
+
+
+    <a href="food_recipes.php">
+        <img src="images/food.png" height="150px" width= "200px"  alt="Go to food recipes page">
+    </a>
+
+
+
+<body>
+</html>
